8448e9c6ea54d83ec2f7efd0cad9b79789e42c0d
Lars Verhaeghe <148888518+larsv2801@users.noreply.github.com>
2025-10-25 10:40:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,17 @@ app.get("/plaatsen", (req, res) => {
 });
 
 app.post("/update", (req, res) => {
-  const {id, x, y } = req.body; // Haal data uit het verzoek
+  const {name, x, y } = req.body; // Haal data uit het verzoek
 
   if (typeof x !== "number" || typeof y !== "number") {
     return res.status(400).json({ error: "x en y moeten nummers zijn!" });
   }
-  const { id, x, y } = req.body;
-  let speler = spelers.find(s => s.id === id);
+  let speler = spelers.find(s => s.name === name);
   if (speler) {
     speler.x = x;
     speler.y = y;
   } else {
-    spelers.push({ id, x, y });
+    spelers.push({ name, x, y });
   }
   res.json({ message: "Score toegevoegd!", spelers });
 });
